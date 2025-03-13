fc35458f5584ab3cbc919c41a888443095f88121
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-03-12 19:05:31 -0700

    workflow instance class

--- durable_snake/workflow.py
+++ durable_snake/workflow.py
@@ -1,13 +1,37 @@
 from enum import Enum
-
 from pydantic import BaseModel
 
 
 class WorkflowStatus(Enum):
+    # Open workflows
     CREATED = "created"
     RUNNING = "running"
 
+    # Closed workflows
+    TERMINATED = "terminated"
+    CONTINUED_AS_NEW = "continued_as_new"
+    CANCELLED = "cancelled"
+    FAILED = "failed"
+    TIMED_OUT = "timed_out"
+
+
 
 class WorkflowInstance(BaseModel):
+    """
+    High level info about a workflow instance
+    """
+
     id: str
+    """The ID of the instance"""
+    type: str
+    """The type of workflow"""
     status: WorkflowStatus
+    queue: str
+    created_ns: int
+    started_ns: int
+    closed_ns: int
+
+    history_length: int = 0
+    history_bytes: int = 0
+    parent_id: str | None = None
+
