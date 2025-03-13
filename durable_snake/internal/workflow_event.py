e81612b52749a25ef2a03cf1528a43d2532ba5ca
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-03-12 20:40:43 -0700

    new workflow event types
    immutable workflow events

--- durable_snake/internal/workflow_event.py
+++ durable_snake/internal/workflow_event.py
@@ -1,18 +1,40 @@
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
-class WorkflowEventStatus(Enum):
-    SCHEDULED = "scheduled"
-    STARTED = "started"
-    COMPLETED = "completed"
-    FAILING = "failing"
-    FAILED = "failed"
+
+class WorkflowEventType(Enum):
+    WORKFLOW_STARTED = "workflow_started"
+    WORKFLOW_FINISHED = "workflow_finished"
+    WORKFLOW_CONTINUED_AS_NEW = "workflow_continued_as_new"
+    WORKFLOW_TERMINATED = "workflow_terminated"
+    WORKFLOW_CANCELED = "workflow_canceled"
+
+    CHILD_WORKFLOW_SCHEDULED = "child_workflow_scheduled"
+    CHILD_WORKFLOW_CANCELLATION_REQUESTED = "child_workflow_cancellation_requested"
+    CHILD_WORKFLOW_COMPLETED = "child_workflow_completed"
+    CHILD_WORKFLOW_FAILED = "child_workflow_failed"
+
+    ACTIVITY_STARTED = "activity_started"
+    ACTIVITY_COMPLETED = "activity_completed"
+    ACTIVITY_FAILED = "activity_failed"
+    ACTIVITY_TIMED_OUT = "activity_timed_out"
+
+    TIMER_SCHEDULED = "timer_scheduled"
+    TIMER_FIRED = "timer_fired"
+    TIMER_CANCELED = "timer_canceled"
+
+    SIGNAL_RECEIVED = "signal_received"
+
+    SIDE_EFFECT_RESULT = "side_effect_result"
+
 
 class WorkflowEvent(BaseModel):
     """
     A workflow event
     """
-    id: int
-    """Sequentially incrementing integer"""
-    status: WorkflowEventStatus
+    model_config = ConfigDict(frozen=True)  # events are immutable
+
+    sequence_id: int
+    """Monotonically increasing sequence number"""
+    type: WorkflowEventType
