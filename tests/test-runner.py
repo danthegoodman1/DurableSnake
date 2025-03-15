3cecc62951e64405e301f4fb4bcdb2238e128383
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-03-15 15:13:18 -0700

    use base backend

--- tests/test-runner.py
+++ tests/test-runner.py
@@ -1,3 +1,4 @@
+from durable_snake.backends.base import BaseBackend
 from durable_snake.runner import Runner, RunnerOptions
 import asyncio
 from loguru import logger
@@ -7,6 +8,6 @@ logger.remove(0) # disable default logger
 logger.add(sys.stdout, level="DEBUG") # set to debug
 
 
-runner = Runner(options=RunnerOptions(id="runner1", queue="queue1", backend=None))
+runner = Runner(options=RunnerOptions(id="runner1", queue="queue1", backend=BaseBackend()))
 
 asyncio.run(runner.start())
